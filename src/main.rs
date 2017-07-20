959a2ad841c3276c071c471149428c34da577ef2
Robby Madruga <robbymadruga@gmail.com>
2017-07-19 21:31:49 -0700

    Improve CLI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![feature(slice_patterns)]
 
+#[macro_use]
 extern crate clap;
-use clap::{App, Arg};
+use clap::{App, AppSettings, Arg, SubCommand};
 
 extern crate serialport;
 use serialport::prelude::*;
@@ -23,7 +24,7 @@ enum Error {
     Serial(serialport::Error),
     IO(io::Error),
     Protocol(protocol::Error),
-    Unknown,
+    Connection,
 }
 
 impl From<serialport::Error> for Error {
@@ -44,9 +45,9 @@ impl From<protocol::Error> for Error {
     }
 }
 
-fn connect(port_name: &str) -> Result<Box<SerialPort>, Error>  {
+fn connect(port_name: &str, baud_rate: usize) -> Result<Box<SerialPort>, Error>  {
     let settings = SerialPortSettings {
-        baud_rate: BaudRate::Baud115200,
+        baud_rate: BaudRate::from_speed(baud_rate),
         data_bits: DataBits::Eight,
         flow_control: FlowControl::None,
         parity: Parity::None,
@@ -102,6 +103,10 @@ fn connect(port_name: &str) -> Result<Box<SerialPort>, Error>  {
         last_error
     }
 
+    {
+        serialport::open_with_settings(port_name, &settings)?;
+    }
+
     print!("Connecting...");
     io::stdout().flush()?;
 
@@ -123,26 +128,81 @@ fn connect(port_name: &str) -> Result<Box<SerialPort>, Error>  {
             },
             Err(_) => {
                 if !delay {
-                    delay = true;
+                    delay = !delay;
                     reset = false;
                 }
             }
         }
     }
 
-    Err(Error::Unknown)
+    println!("");
+    Err(Error::Connection)
+}
+
+fn build_cli() -> App<'static, 'static> {
+    App::new("esptool")
+        .author("Robby Madruga <robbymadruga@gmail.com>")
+        .version("v0.0.1")
+        .setting(AppSettings::SubcommandRequired)
+        .arg(Arg::with_name("CHIP")
+            .takes_value(true)
+            .possible_values(&["auto", "esp8266", "esp32"])
+            .long("chip")
+        )
+        .arg(Arg::with_name("PORT")
+            .takes_value(true)
+            .long("port")
+        )
+        .arg(Arg::with_name("BAUD")
+            .takes_value(true)
+            .long("baud")
+            .required(false)
+        )
+        .subcommand(SubCommand::with_name("load_ram").about("Download an image to RAM and execute"))
+        .subcommand(SubCommand::with_name("dump_mem").about("Dump arbitrary memory to disk"))
+        .subcommand(SubCommand::with_name("read_mem").about("Read arbitrary memory location"))
+        .subcommand(SubCommand::with_name("write_mem").about("Read-modify-write to arbitrary memory location"))
+        .subcommand(SubCommand::with_name("write_flash").about("Write a binary blob to flash"))
+        .subcommand(SubCommand::with_name("run").about("Run application code in flash"))
+        .subcommand(SubCommand::with_name("image_info").about("Dump headers from an application image"))
+        .subcommand(SubCommand::with_name("make_image").about("Create an application image from binary files"))
+        .subcommand(SubCommand::with_name("elf2image").about("Create an application image from ELF file"))
+        .subcommand(SubCommand::with_name("read_mac").about("Read MAC address from OTP ROM"))
+        .subcommand(SubCommand::with_name("chip_id").about("Read Chip ID from OTP ROM"))
+        .subcommand(SubCommand::with_name("flash_id").about("Read SPI flash manufacturer and device ID"))
+        .subcommand(SubCommand::with_name("read_flash_status").about("Read SPI flash status register"))
+        .subcommand(SubCommand::with_name("write_flash_status").about("Write SPI flash status register"))
+        .subcommand(SubCommand::with_name("read_flash").about("Read SPI flash content"))
+        .subcommand(SubCommand::with_name("verify_flash").about("Verify a binary blob against flash"))
+        .subcommand(SubCommand::with_name("erase_flash").about("Perform Chip Erase on SPI flash"))
+        .subcommand(SubCommand::with_name("erase_region").about("Erase a region of the flash"))
 }
 
 fn main() {
-    let args = App::new("esptool")
-                .arg(Arg::with_name("port")
-                    .required(true)
-                )
-                .get_matches();
+    let args = build_cli().get_matches();
     
-    let port_name = args.value_of("port").unwrap();
-    let mut port = connect(port_name).unwrap();
-    let value = port.read_reg(Register::UartDataReg).unwrap();
-    let value = port.read_reg(Register::UartDataReg).unwrap();
-    println!("Value: {:X}", value);
+    let port_name = args.value_of("PORT").unwrap_or("/dev/ttyUSB0");
+    let baud_rate = value_t!(args.value_of("BAUD"), usize)
+                        .unwrap_or_else(|e| match e.kind {
+                            clap::ErrorKind::ArgumentNotFound => 115200,
+                            _ => e.exit(),
+                        });
+
+    match connect(port_name, baud_rate) {
+        Ok(port) => {
+
+        },
+        Err(Error::Serial(error)) => {
+            println!("Serial Error: {}", error);
+        },
+        Err(Error::IO(error)) => {
+            println!("IO Error: {}", error);
+        },
+        Err(Error::Protocol(error)) => {
+            println!("Protocol Error: {:?}", error);
+        },
+        Err(Error::Connection) => {
+            println!("Connection Error: Unable to connect to device");
+        }
+    }
 }
